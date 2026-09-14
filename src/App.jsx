07500d4385ab1f3fda52a57ca28669645d6fbd80
Vreekly <kuba.sperka@wp.pl>
2026-09-14 12:34:15 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css'
-import { useState } from 'react'
-import "../bootstrap/css/bootstrap.css"
+import Biblioteka from './components/MyFirstComponent'
 
 function App() {
-  const [name, setName] = useState("")
 
   return (
-    <div style={{margin: 20}}>
-      <div className='form-control'>
-      <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input> <br></br>
-      <input type='button' className='btn btn-primary' value='Reset' onClick={(e) => setName("")}></input>
-      </div>
-
-      <p style={{ fontSize: 20 }}>Hello, {name}</p>
-    </div>
+    <Biblioteka></Biblioteka>
   )
 }
 
---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import "../bootstrap/css/bootstrap.css"
+
+function Biblioteka() {
+  const books = ["Wiedźmin", "Pan Tadeusz", "Lalka", "Solaris"];
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState(0);
+
+  return (
+    <div>
+      <h2>Dostępne książki: {books.length}</h2> <br></br>
+      <div className='text-start'>
+      <ol>
+      {books.map((element) => {
+          return <li>{element}</li>
+      })}
+      </ol>
+      </div>
+
+      <div className='form-control'>
+        <label><b>Imie czytelnika:</b> </label> <input type='text' onChange={((e) => setName(e.value.target))}></input> <br></br> <br></br>
+        <label><b>Numer ksiażki:</b> </label> <input type='number' onChange={((e) => setNumber(e.value.target))}></input> <br></br> <br></br>
+        <input type='button' value='Wypożycz'></input> 
+      </div>
+
+      {if()}
+      
+    
+
+    </div>
+  )
+}
+
+export default Biblioteka
