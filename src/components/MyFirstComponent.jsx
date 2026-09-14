d1a78f919d3f7e3864139bd80dd035fd7f99d118
Vreekly <kuba.sperka@wp.pl>
2026-09-14 12:58:15 +0200

    Update

--- src/components/MyFirstComponent.jsx
+++ src/components/MyFirstComponent.jsx
@@ -2,28 +2,57 @@ import { useState } from 'react'
 import "../bootstrap/css/bootstrap.css"
 
 function Biblioteka() {
-  const books = ["Wiedźmin", "Pan Tadeusz", "Lalka", "Solaris"];
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState(0);
+  
+  const books = ["Wiedźmin", "Pan Tadeusz", "Lalka", "Solaris"]
+
+  const [readerName, setReaderName] = useState("")
+  const [bookNumber, setBookNumber] = useState("")
+
+  const borrowBook = () => {
+    const number = parseInt(bookNumber)
+
+    if (number >= 1 && number <= books.length) {
+      console.log(
+        `Wypożyczenie: ${readerName}, książka: ${books[number - 1]}`
+      )
+    } else {
+      console.log("Nieprawidłowy numer książki")
+    }
+  }
 
   return (
-    <div>
-      <h2>Dostępne książki: {books.length}</h2> <br></br>
-      <div className='text-start'>
-      <ol>
-      {books.map((element) => {
-          return <li>{element}</li>
-      })}
-      </ol>
-      </div>
-
-      <div className='form-control'>
-        <label><b>Imie czytelnika:</b> </label> <input type='text' onChange={((e) => setName(e.value.target))}></input> <br></br> <br></br>
-        <label><b>Numer ksiażki:</b> </label> <input type='number' onChange={((e) => setNumber(e.value.target))}></input> <br></br> <br></br>
-        <input type='button' value='Wypożycz'></input> 
-      </div>
+    <div className="p-3">
       
-    
+      <h2 className="text-start mb-4">Dostępne książki: {books.length}</h2>
+
+      <ol className="text-start">
+        {books.map((book, index) => (
+          <li key={index}>{book}</li>
+        ))}
+      </ol>
+
+      <form className="mt-4">
+
+        <div className="mb-3">
+
+          <label htmlFor="readerName" className="form-label">Imię czytelnika:</label>
+
+          <input type="text" className="form-control" value={readerName} onChange={(e) => setReaderName(e.target.value)}/>
+       
+        </div>
+
+        <div className="mb-3">
+
+          <label htmlFor="bookNumber" className="form-label">Numer książki:</label>
+
+          <input type="number" id="bookNumber" className="form-control" value={bookNumber} onChange={(e) => setBookNumber(e.target.value)}/>
+       
+        </div>
+
+        <button type="button" className="btn btn-primary" onClick={borrowBook}>Wypożycz</button>
+
+     
+      </form>
 
     </div>
   )
